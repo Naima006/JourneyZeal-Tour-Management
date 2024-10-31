937c41ded1334daf4cc2a341b354a132ccf20a8f
Naima Rahman <naimarahman006@gmail.com>
2024-10-31 22:34:57 +0600

    Complete frontend

--- tour-management/frontend/src/components/Footer/Footer.jsx
+++ tour-management/frontend/src/components/Footer/Footer.jsx
@@ -105,7 +105,7 @@ const Footer = () => {
                                     <span><i class="ri-map-pin-line"></i></span>
                                     Address:
                                 </h6>
-                                <p className="mb-0">Sylhet, Bangladesh</p>
+                                <p className="mb-0">Sylhet,Bangladesh.</p>
                             </ListGroupItem>
 
                             <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
