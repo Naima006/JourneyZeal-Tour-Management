d83bf5abd9a622b7538a62a1ec500512d5cd0518
Khadiza Sultana Chowdhury Rimi <chowdhuryrimi59@gmail.com>
2024-11-02 19:23:40 +0600

    Fetch Data

--- tour-management/frontend/src/index.js
+++ tour-management/frontend/src/index.js
@@ -7,12 +7,15 @@ import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
--- tour-management/frontend/src/pages/Tours.jsx
+++ tour-management/frontend/src/pages/Tours.jsx
@@ -6,57 +6,71 @@ import "../styles/tours.css";
 import Newsletter from "./../shared/Newsletter";
 import SearchBar from "./../shared/SearchBar";
 import TourCard from "./../shared/TourCard";
+import useFetch from "../hooks/useFetch";
+import { BASE_URL } from "../utils/config";
 
 const Tours = () => {
-    const [pageCount, setPageCount] = useState(0);
-    const [page, setPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
 
-    useEffect(() => {
-        const pages = Math.ceil(5 / 4); //use backend data count
-        setPageCount(pages);
-    }, [page]);
+  const {
+    data: tours,
+    loading,
+    error,
+  } = useFetch(`${BASE_URL}/tours?page=${page}`);
+  const { data: tourCount } = useFetch(`${BASE_URL}/tours/search/getTourCount`);
 
-    return (
-        <>
-            <CommonSection title={"All Tours"} />
-            <section>
-                <Container>
-                    <Row>
-                        <SearchBar />
-                    </Row>
-                </Container>
-            </section>
-            <section className="pt-0">
-                <Container>
-                    <Row>
-                        {tourData?.map((tour) => (
-                            <Col lg="3" className="mb-4" key={tour.id}>
-                                <TourCard tour={tour} />
-                            </Col>
-                        ))}
+  useEffect(() => {
+    const pages = Math.ceil(tourCount / 8);
+    setPageCount(pages);
+    window.scrollTo(0, 0);
+  }, [page, tourCount, tours]);
 
-                        <Col lg="12">
-                            <div
-                                className="pagination d-flex align-content-center 
+  return (
+    <>
+      <CommonSection title={"All Tours"} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          {loading && <h4 className="text-center pt-5">Loading.....</h4>}
+          {error && <h4 className="text-center pt-5">{error}</h4>}
+          {!loading && !error && (
+            <Row>
+              {tours?.map((tour) => (
+                <Col lg="3" className="mb-4" key={tour._id}>
+                  <TourCard tour={tour} />
+                </Col>
+              ))}
+
+              <Col lg="12">
+                <div
+                  className="pagination d-flex align-content-center 
                             justify-content-center mt-4 gap-3"
-                            >
-                                {[...Array(pageCount).keys()].map((number) => (
-                                    <span
-                                        key={number}
-                                        onClick={() => setPage(number)}
-                                        className={page === number ? "active__page" : ""}
-                                    >
-                                        {number + 1}
-                                    </span>
-                                ))}
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-            <Newsletter />
-        </>
-    );
+                >
+                  {[...Array(pageCount).keys()].map((number) => (
+                    <span
+                      key={number}
+                      onClick={() => setPage(number)}
+                      className={page === number ? "active__page" : ""}
+                    >
+                      {number + 1}
+                    </span>
+                  ))}
+                </div>
+              </Col>
+            </Row>
+          )}
+        </Container>
+      </section>
+      <Newsletter />
+    </>
+  );
 };
 
 export default Tours;
